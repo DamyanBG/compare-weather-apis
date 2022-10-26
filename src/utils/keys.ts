ceb5f51fd8228f56e2275dbb989a5d9edd607228
Damyan Dimitrov <d.dimitrov.1988@abv.bg>
2022-10-26 23:05:25 +0300

    add weatherstack

--- src/utils/keys.ts
+++ src/utils/keys.ts
@@ -1,3 +1,4 @@
 export const WEATHER_API_KEY: string | undefined = process.env.REACT_APP_WEATHER_API_KEY
 export const OPEN_WEATHER_API_KEY: string | undefined = process.env.REACT_APP_OPEN_WEATHER_KEY
 export const ACCU_WEATHER_API_KEY: string | undefined = process.env.REACT_APP_ACCU_WEATHER_KEY
+export const WEATHERSTACK_API_KEY: string | undefined = process.env.REACT_APP_WEATHERSTACK_KEY
